754d30b9a0551188cedeb54447dba823f72ab680
alexdupreez1 <alexandros.dupreez@gmail.com>
2024-06-12 16:02:22 +0200

    Added fundamentalist trader

---
+++ trader.py
@@ -0,0 +1,57 @@
+
+
+
+class Fundamentalist():
+
+    def __init__(self, fundamental_value, prev_price):
+
+        self.fundamental_value = fundamental_value
+        self.prev_price = prev_price
+
+        #To be initialised later 
+        self.mt = 0
+        self.MT = 0
+        self.demand = 0
+        
+    def compute_price_boundaries(self):
+
+        k = 1.1 #Preselected factor
+
+        self.mt = (1/k)*self.fundamental_value
+
+        self.MT = k*self.fundamental_value
+        
+
+    def determine_chance_function(self):
+        
+        #Parameters that describe the sensitiveness of fundamentalists
+        a = 0 
+        d = 0
+
+        self.compute_price_boundaries() #Calculate values of mt and MT
+
+        #Compute value of chance function (given by A)
+        A = (a(self.prev_price - self.mt)**d)*((self.MT - self.prev_price)**d)
+
+        return A
+
+
+
+    def determine_demand(self):
+
+        price_zone = (self.mt,self.MT)
+
+        if(self.prev_price in price_zone):
+
+            self.demand = (self.fundamental_value - self.prev_price)()
+
+        else:
+
+            self.demand = 0 
+        
+        
+
+        
+
+
+
